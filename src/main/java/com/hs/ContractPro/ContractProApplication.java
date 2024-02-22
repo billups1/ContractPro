aa5767ed8830489f3785bbb9e40816273d14195d
billups1 <eglesv2@hanmail.net>
2024-02-23 01:11:11 +0900

    첫번째 커밋

---
+++ src/main/java/com/hs/ContractPro/ContractProApplication.java
@@ -0,0 +1,13 @@
+package com.hs.ContractPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hs/ContractPro/ContractProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hs.ContractPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
